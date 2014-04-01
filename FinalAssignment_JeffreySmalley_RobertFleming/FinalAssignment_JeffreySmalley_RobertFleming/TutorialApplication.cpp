ed320785af6f61c774d0014da25f3a0779645e84
Flembottles <daflemdaga@gmail.com>
2014-04-01 18:47:21 -0400

    Update - Cannot Find DualQuaternion

--- FinalAssignment_JeffreySmalley_RobertFleming/FinalAssignment_JeffreySmalley_RobertFleming/TutorialApplication.cpp
+++ FinalAssignment_JeffreySmalley_RobertFleming/FinalAssignment_JeffreySmalley_RobertFleming/TutorialApplication.cpp
@@ -28,7 +28,15 @@ TutorialApplication::~TutorialApplication(void)
 //-------------------------------------------------------------------------------------
 void TutorialApplication::createScene(void)
 {
-    // create your scene here :)
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5f,0.5f,0.5f));
+
+	Ogre::Entity* ogreHead = mSceneMgr->createEntity("Head", "ogrehead.mesh");
+
+	Ogre::SceneNode* headNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("HeadNode");
+	headNode->attachObject(ogreHead);
+
+	Ogre::Light* light = mSceneMgr->createLight("MainLight");
+	light->setPosition(20.0f, 80.0f, 50.0f);
 }
 
 
--- FinalAssignment_JeffreySmalley_RobertFleming/FinalAssignment_JeffreySmalley_RobertFleming/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <Ogre.h>
-int main(int argc, char* argv[])
-{
-    Ogre::Root* root = new Ogre::Root("plugins.cfg", "ogre.cfg", "Ogre.log");
-    delete root;
-    system("pause");
-}
